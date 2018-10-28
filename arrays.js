ac69eb9d5ba483ea384cf0d18f16bd548ab1e6b3
Pnina Breslav <pcrbus123@gmail.com>
2018-10-28 13:13:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,14 +1,3 @@
 const chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 console.log(chocolateBars);
 
-function addElementToBeginningOfArray(array, element){
-    var myarray = [array];
-newArray= [element,...myarray];
- return (newArray);
-};
-
-function destructivelyAddElementToBeginningOfArray (array, element){
-var myArray = [array];
- myArray.unshift(element);
-return myArray);
-}
