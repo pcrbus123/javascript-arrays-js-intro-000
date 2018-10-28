7498ac955b9888a7eb4b7a0fc761d86eeeab0e1c
Pnina Breslav <pcrbus123@gmail.com>
2018-10-28 13:14:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,14 @@
-const chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-console.log(chocolateBars);
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+return chocolateBars;
+
+function addElementToBeginningOfArray(array, element){
+    var myarray = [array];
+newArray= [element,...myarray];
+ return (newArray);
+};
+
+function destructivelyAddElementToBeginningOfArray (array, element){
+var myArray = [array];
+ myArray.unshift(element);
+return myArray);
+}
